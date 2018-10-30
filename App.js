947f325a7afd2af6a950050f9f7e012e729c5a92
Tyler Bray <broncobray77@gmail.com>
2018-10-30 10:03:05 -0700

    init

---
+++ App.js
@@ -0,0 +1,81 @@
+import React from "react";
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  TouchableHighlight
+} from "react-native";
+import t from "tcomb-form-native";
+
+var Form = t.form.Form;
+
+// here we are: define your domain model
+var Person = t.struct({
+  name: t.String, // a required string
+  surname: t.maybe(t.String), // an optional string
+  age: t.Number, // a required number
+  Country: t.String,
+  rememberMe: t.Boolean, // a boolean
+});
+
+var options = {}; // optional rendering options (see documentation)
+
+class AwesomeProject extends React.Component {
+  constructor(props) {
+    super(props);
+    //this.onPress = this.bind.onPress(this);
+    this.onPress = this.onPress.bind(this);
+  }
+
+  onPress() {
+    // call getValue() to get the values of the form
+    var value = this.refs.form.getValue();
+    if (value) {
+      // if validation fails, value will be null
+      console.log(value); // value here is an instance of Person
+    }
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        {/* display */}
+        <Form ref="form" type={Person} options={options} />
+        <TouchableHighlight
+          style={styles.button}
+          onPress={this.onPress}
+          underlayColor="#99d9f4"
+        >
+          <Text style={styles.buttonText}>Save</Text>
+        </TouchableHighlight>
+      </View>
+    );
+  }
+}
+
+export default AwesomeProject;
+
+var styles = StyleSheet.create({
+  container: {
+    justifyContent: "center",
+    marginTop: 50,
+    padding: 20,
+    backgroundColor: "#ffffff"
+  },
+  buttonText: {
+    fontSize: 18,
+    color: "white",
+    alignSelf: "center"
+  },
+  button: {
+    height: 36,
+    backgroundColor: "#48BBEC",
+    borderColor: "#48BBEC",
+    borderWidth: 1,
+    borderRadius: 8,
+    marginBottom: 10,
+    alignSelf: "stretch",
+    justifyContent: "center"
+  }
+});
